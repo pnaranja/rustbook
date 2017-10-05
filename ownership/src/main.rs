3ebefc5ff9521976fe51386546bc77d13db458e3
pnaranja <paul.naranja@gmail.com>
2017-10-05 00:22:39 -0700

    Finished chp4 - ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -19,7 +19,26 @@ fn main() {
     println!("x: {}, y: {}", x, y);
     println!("x2: {}, y2: {}", x2, y2);
 
-    ownership_test()
+    ownership_test();
+
+    // calculate length is borrowing the s1 variable?
+    let s1_length = calculate_length(&s1);
+    println!("Length of String {} is {}", s1, s1_length);
+
+    let mut m1 = String::from("Paul");
+    let m1_length = calculate_length_mutate(&mut m1);
+    println!("Length of String {} is {}", m1, m1_length);
+
+    // Can't mix mut and immutable refs
+    let m2 = &m1;
+    let m3 = &m1;
+    //let m4 = &mut m1;
+    println!("{} is {}", m2, m3);
+
+    // No dangling refs
+    let good_ref = get_ref();
+    println!("{}",good_ref)
+
 }
 
 fn ownership_test()
@@ -45,3 +64,24 @@ fn function_copy(i : i32)
     let a = i + 3;
     println!("Copied the number {} and added 3 to get {}", i, a)
 }
+
+fn calculate_length(s: &String) -> usize
+{
+//    Cannot mutate a reference
+//    s.push_str(" and hello");
+    s.len()
+}
+
+fn calculate_length_mutate(s : &mut String) -> usize
+{
+//    Can mutate a mutable reference
+    s.push_str(" Orange");
+    s.len()
+}
+
+fn get_ref() -> String
+{
+    let s = String::from("Bye!");
+    s
+}
+
