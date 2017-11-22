32f7b11f33102bd79efa6518d1062dd35972ad39
pnaranja <paul.naranja@gmail.com>
2017-11-21 16:52:47 -0800

    Adding error handling chapter

---
+++ error_handling/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+use std::io::Read;
+use std::io::Write;
+use std::io;
+
+fn main() {
+    open_or_create_file();
+    open_or_create_file2();
+    let mut f = ret_result().unwrap_or_else(|_| panic!("There was a problem with ret_result"));
+
+    f.write("Hello everyone!".as_bytes()).unwrap();
+}
+
+fn open_or_create_file ()
+{
+    let f = match (File::open ("test/hello.txt")) {
+        Ok (file) => file,
+        Err (ref err) if err.kind() == ErrorKind::NotFound =>{
+            match File::create ("test/hello.txt"){
+                Ok (fc) => fc,
+                Err (err2) => panic! ("Could not create file")
+            }
+        }
+        Err (error) => panic! ("HUGE Problem! : {:?}", error)
+    };
+
+}
+
+fn open_or_create_file2 ()
+{
+    let f = File::open("test2/hello.txt")
+        .unwrap_or_else(|_| File::create("test2/hello.txt")
+        .unwrap_or_else(|_| panic!("Huge Problem")));
+}
+
+fn ret_result() -> Result<File,io::Error>
+{
+    let mut f = File::create("test2/hello.txt");
+    match f{
+        Ok(f) => Ok(f),
+        Err(e) => Err(e)
+    }
+}
+
