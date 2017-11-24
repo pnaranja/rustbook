f0f9e11b6fe51c67e1a8c6b83cec2a7db75f9083
pnaranja <paul.naranja@gmail.com>
2017-11-24 01:45:41 -0800

    Use Generic to get largest element in list

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ord;
+
+
 fn largest_i32(list : &[i32]) -> i32
 {
     list.iter().fold(0, |acc,&b| if acc>b {acc.clone()} else {b.clone()})
@@ -8,10 +11,22 @@ fn largest_char(list : &[char]) -> char
     list.iter().fold('a', |acc,&b| if acc>b {acc.clone()} else {b.clone()})
 }
 
+fn largest<T : Clone + Ord >(list : &[T]) -> T
+{
+    let mut lst = list.to_owned();
+    lst.sort();
+    lst.last().unwrap().clone()
+}
+
 fn main() {
     let num_list = vec![1,4,5,3,9,3,4,6,8];
     let char_list = vec!['a','b','z','d','y','p'];
 
     println!("Largest number is {}", largest_i32(&num_list));
     println!("Largest char is {}", largest_char(&char_list));
+
+    println!("Using Generic: Largest number is {}", largest(&num_list));
+    println!("Using Generic: Largest char is {}", largest(&char_list));
+
+
 }
