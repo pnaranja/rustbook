3b24efe4be126c08af968b72842c6f1b9c1c85d9
pnaranja <paul.naranja@gmail.com>
2017-12-10 14:28:09 -0800

    Start testing section

---
+++ testing/src/lib.rs
@@ -0,0 +1,40 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    //#[test]
+    //fn will_fail() {
+    //    panic!("Failing test");
+    //}
+
+    fn larger_can_hold_smaller(){
+        let larger = Rectangle {length: 8, width: 7};
+        let smaller = Rectangle {length: 5, width: 1};
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[derive(Debug)]
+    struct Rectangle{
+        length: u32,
+        width: u32
+    }
+
+    impl Rectangle{
+        // Parameter is a reference to itself since the method is tied to the instance of the Rectangle2 struct
+        fn area(&self) -> u32{
+            self.length * self.width
+        }
+
+        fn can_hold(&self, other_rect : &Rectangle) -> bool{
+            (self.width > other_rect.width) && (self.length > other_rect.length)
+        }
+    }
+
+
+}
+
+
