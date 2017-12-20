f0582dcfe718e6fa968b9b1033b0dfe368a7dd57
pnaranja <paul.naranja@gmail.com>
2017-12-20 12:52:25 -0800

    Added another test

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -11,10 +11,18 @@ mod tests {
     //    panic!("Failing test");
     //}
 
+    #[test]
     fn larger_can_hold_smaller(){
         let larger = Rectangle {length: 8, width: 7};
         let smaller = Rectangle {length: 5, width: 1};
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger(){
+        let larger = Rectangle {length: 8, width: 7};
+        let smaller = Rectangle {length: 5, width: 1};
+        assert!(!smaller.can_hold(&smaller));
     }
 
     #[derive(Debug)]
