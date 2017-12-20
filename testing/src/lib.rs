801e654f22cbd119bb16bfc70025006c47e84a1f
pnaranja <paul.naranja@gmail.com>
2017-12-20 13:42:54 -0800

    Finish section 11.1

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -22,8 +22,55 @@ mod tests {
     fn smaller_cannot_hold_larger(){
         let larger = Rectangle {length: 8, width: 7};
         let smaller = Rectangle {length: 5, width: 1};
-        assert!(!smaller.can_hold(&smaller));
+        assert!(!smaller.can_hold(&larger));
     }
+
+
+    fn greeting(name: &str) -> String{
+        format!("Hello {}!", name)
+    }
+
+    #[test]
+    fn greeting_test(){
+        assert_eq!(greeting("Paul"), "Hello Paul!")
+    }
+
+    // thread 'tests::greeting_test_error' panicked at 'Expected 'Hello Paul!' but received 'Hello Mike!''
+    #[test]
+    fn greeting_test_error(){
+        assert!(greeting("Mike").eq("Hello Paul!"), "Expected 'Hello Paul!' but received '{}'", greeting("Mike"))
+    }
+
+
+
+    fn not_greater_than_100(value: i32) -> i32{
+        if value > 100 {panic!("Cannot be greater than 100")}
+        value
+    }
+
+    // Verify panics
+    #[test]
+    #[should_panic]
+    fn greater_than_100(){
+        not_greater_than_100(101);
+    }
+
+    #[test]
+    #[should_panic]
+    fn less_than_100(){
+        not_greater_than_100(99);
+    }
+
+    #[test]
+    #[should_panic(expected="Cannot be something")]
+    fn wrong_panic_msg(){
+        not_greater_than_100(102);
+    }
+
+
+
+
+
 
     #[derive(Debug)]
     struct Rectangle{
