404bf5d800c485a08f2191e1acac7b74768527e1
pnaranja <paul.naranja@gmail.com>
2017-12-22 11:24:01 -0800

    Add generic function to 'exit gracefully'

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,14 @@
-use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::io::prelude;
 use std::process;
 use std::error::Error;
 
+fn exit_gracefully<E: std::fmt::Debug, T: std::fmt::Debug>(msg: E) -> T{
+    println!("Problem parsing arguments: {:?}", msg);
+    process::exit(1);
+}
+
+#[derive(Debug)]
 pub struct Config{
     query: String,
     filename : String
@@ -13,31 +17,32 @@ pub struct Config{
 impl Config{
     /// Creates a Config struct of the parameters
     pub fn new(args: &[String]) -> Config {
-        Config::parse_args(args)
-            .unwrap_or_else(|err| {
-                println!("Problem parsing arguments: {}", err);
-                process::exit(1);
-            })
+        Config::parse_args(args).unwrap_or_else(exit_gracefully)
     }
 
     fn parse_args (args: &[String]) -> Result<Config, &'static str>{
         if args.len() < 3 {return Err ("Not enough arguments");}
+        if args.len() > 3 {return Err ("Too many arguments");}
         Ok (Config{query : args[1].clone() , filename : args[2].clone() })
     }
 }
 
+/// Retrieve the contents of the file in the config
 pub fn get_contents(config: Config) -> String {
     read_from_contents(config)
-                    .unwrap_or_else(|err| {
-                        println!("Application error: {}", err);
-                        process::exit(1);
-                    })
+        .unwrap_or_else(|err| {
+            println!("Application error: {}", err);
+            process::exit(1);
+        })
 }
 
 fn read_from_contents(config: Config) -> Result<String, Box<Error>> {
-    let mut f = File::open(config.filename).expect("File not found");
+    let mut f = open_file(config);
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
     Ok(contents)
 }
 
+fn open_file (config: Config) -> File{
+    File::open(config.filename).unwrap_or_else(exit_gracefully)
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,3 +11,9 @@ fn main() {
 
     println!("Here is the contents of the file '{}':\n\n{}",args[2], get_contents(config));
 }
+
+#[test]
+fn too_little_arguments (){
+    let args :Vec<String> = vec!(String::from("hello"));
+    Config::new(&args);
+}
