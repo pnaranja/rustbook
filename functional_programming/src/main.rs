c4dd8bf95fdc040ac3f2fab35a9cb1bdd2df489b
pnaranja <paul.naranja@gmail.com>
2017-12-27 18:53:32 -0800

    Memoized calculation function

--- functional_programming/src/main.rs
+++ functional_programming/src/main.rs
@@ -4,8 +4,30 @@ use std::thread;
 use std::time::Duration;
 use rand::Rng;
 
+struct Memoizer<T> where T: Fn(i32) -> i32{
+    calculation : T,
+    value: Option<i32>
+}
+
+impl<T> Memoizer<T> where T:Fn(i32) -> i32{
+    fn new(calc_func : T) -> Memoizer<T>{
+        Memoizer{calculation: calc_func, value: None}
+    }
+
+    pub fn get_value(&mut self, intensity: i32) -> i32{
+        match self.value{
+            Some(_v) => intensity,
+            None => {
+                let ret = (self.calculation)(intensity);
+                self.value = Some(ret);
+                ret
+            }
+        }
+    }
+}
+
 fn simulated_expensive_calculation(intensity: i32) -> i32 {
-    let random_wait_secs = rand::thread_rng().gen_range (1,11);
+    let random_wait_secs = rand::thread_rng().gen_range (3,6); //random number between 3-5
 
     println!("Calculating slowly");
     thread::sleep(Duration::from_secs(random_wait_secs));
@@ -13,35 +35,26 @@ fn simulated_expensive_calculation(intensity: i32) -> i32 {
 }
 
 // Refactor function to only need to run expensive calculation once
-fn generate_workout(intensity: i32){
-    let expensive_closure = |intensity| {
-        let random_wait_secs = rand::thread_rng().gen_range (1,11);
-
-        println!("Calculating slowly");
-        thread::sleep(Duration::from_secs(random_wait_secs));
-        intensity
-    };
-
+fn generate_workout<T:Fn(i32) -> i32>(calculation: T, intensity: i32){
+    let mut memoized_func = Memoizer::new(calculation);
 
     if intensity < 25{
-        println!("Do pushups!");
-        simulated_expensive_calculation(intensity);
-        println!("Do situps!");
-        simulated_expensive_calculation(intensity);
+        println!("Do {} pushups!", memoized_func.get_value(intensity));
+
+        println!("Do {} situps!", memoized_func.get_value(intensity));
     }
     else {
-        // The expensive calculation should not be called at all if in this if block
+        // The expensive calculation should not be called at all, in this if block
         if intensity%2==0{
             println!("Take a break today!");
         }
         else{
-            println!("Run for {} minutes", intensity);
-            simulated_expensive_calculation(intensity);
+            println!("Run for {} minutes", memoized_func.get_value(intensity));
         }
     }
 }
 
 fn main(){
-    generate_workout(29);
+    generate_workout(simulated_expensive_calculation, 23);
 
 }
