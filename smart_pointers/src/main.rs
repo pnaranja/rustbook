fc7cd48a57c7e06d51f029e7ab3d03c521dcb599
pnaranja <paul.naranja@gmail.com>
2018-01-10 19:12:23 -0800

    Finish section 15.4 - Ref counts

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -80,8 +80,51 @@ impl<T : Debug> Drop for myBox<T>{
     }
 }
 
+
+//Using Rc for the List
+use std::rc::Rc;
+use RcList::{RcCons, RcNil};
+
+enum RcList{
+    RcCons(i32, Rc<RcList>),
+    RcNil
+}
+
+
+/// Trying to create two lists that share ownership of a third list
+fn three_lists(){
+    let _a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let _b = Cons(3, Box::new(_a));
+
+    // Won't work because a was already moved to b
+//    let _c = Cons(4, Box::new(_a));
+}
+
+/// Using Rc allows a single value ("_a") to have multiple owners (via immutable refs)
+/// Using Rc::clone increasing the reference count to a
+/// We could have used a2.clone() but that would have been a deep copy
+fn three_lists_using_Rc(){
+
+    let __a = RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil))));
+    let _a = Rc::new(__a); //need to create a new instance of Rc of the RcList in order for Rc::clone
+    println!("\nRef count after creating _a: {}", Rc::strong_count(&_a));
+
+    let _b = RcCons(3, Rc::clone(&_a));
+    println!("Ref count after creating _b: {}", Rc::strong_count(&_a));
+
+    {
+        let _c = RcCons(4, Rc::clone(&_a));
+        println!("Ref count after creating _c: {}", Rc::strong_count(&_a));
+
+    }
+    println!("Ref count after _c is out of scope: {}", Rc::strong_count(&_a));
+
+}
+
 fn main (){
     boxes();
     use_my_box();
     implicit_deref_coercion();
+    three_lists();
+    three_lists_using_Rc();
 }
