49aefe0e1c8f12e7c75fcab18fb54b2c1b73f7c7
pnaranja <paul.naranja@gmail.com>
2018-03-29 17:37:18 -0700

    Add Approve, Publish states and get content

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -12,51 +12,85 @@ pub struct Post{
 
 impl Post{
     fn new() -> Post{
-        // A new blog post will start with a draft state
+        /// A new blog post will start with a draft state
         Post{
             state : Some (Box::new(Draft {})),
             content : String::new()
         }
     }
 
-    // Need a mutating self reference since content is changing
+    /// Need a mutating self reference since content is changing
     pub fn add_text(&mut self, text : &str){
         self.content.push_str(text);
     }
 
-    // PlaceHolder - Return empty string for now.  Later need to check state
-    pub fn content(&self) -> &str{
-        ""
+    /// PlaceHolder - Return empty string for now.  Later need to check state
+    pub fn content(&mut self) -> &str{
+        self.state.take().unwrap_or_else(||process::exit(1)).content(self)
     }
 
-    // take() will return the Option<Box<State>> and replace self.state with None using
-    // core::mem::replace(self,None) - https://doc.rust-lang.org/core/mem/fn.replace.html
+    /// take() will return the Option<Box<State>> and replace self.state with None using
+    /// core::mem::replace(self,None) - https://doc.rust-lang.org/core/mem/fn.replace.html
     pub fn request_review(&mut self){
         self.state = Some(self.state.take().unwrap_or_else(||process::exit(1)).request_review());
+    }
+
+    pub fn approve(&mut self){
+        self.state = Some(self.state.take().unwrap_or_else(||process::exit(1)).approve());
     }
 
 }
 
 trait State {
     fn request_review(self : Box<Self>) -> Box<State>;
+    fn approve(self: Box<Self>) -> Box<State>;
+    fn content<'a>(&self, post: &'a Post)->&'a str;
 }
 
-// Represent the Draft State
+/// Represent the Draft State
 struct Draft {}
 
 impl State for Draft {
-    // Requesting a review for a draft returns an pending review state
+    /// Requesting a review for a draft returns an pending review state
     fn request_review (self:Box<Self>) -> Box<State>{
         Box::new(PendingReview {})
     }
+
+    /// Cannot approve a draft.  Needs to be reviewed
+    fn approve(self: Box<Self>) -> Box<State>{ self }
+
+    /// Unapproved content returns an empty string
+    fn content<'a>(&self, post: &'a Post) -> &'a str{""}
 }
 
-// Represent the Pending Review State
+/// Represent the Pending Review State
 struct PendingReview {}
 
 impl State for PendingReview {
-    // For now? request a review for pending review state returns itself
+    /// request a review for pending review state returns itself, since it's already in review!
     fn request_review (self:Box<Self>) -> Box<State>{
         self
+    }
+
+    /// Approve a pending review should return a publish state
+    fn approve(self: Box<Self>) -> Box<State>{ Box::new(Publish {}) }
+
+    /// Unapproved content returns an empty string
+    fn content<'a>(&self, post: &'a Post) -> &'a str{""}
+}
+
+/// Represent the Published state.  This is when a Pending Review gets approved
+struct Publish {}
+
+impl State for Publish{
+    /// requesting a review or approving a post that is already published doesn't make any sense
+    fn request_review(self : Box<Self>) -> Box<State>{ self }
+
+    /// requesting a review or approving a post that is already published doesn't make any sense
+    fn approve(self : Box<Self>) -> Box<State>{ self }
+
+    /// Return published content
+    fn content<'a>(&self, post: &'a Post) -> &'a str{
+        post.content.as_ref()
     }
 }
