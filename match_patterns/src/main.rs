7f918bf26e4186d6305b18acee8cd71c5bcfaf9f
Paul Naranja <paul@studyblue.com>
2018-07-06 17:08:36 -0700

    Add more destructuring patterns and ignore value examples

--- match_patterns/src/main.rs
+++ match_patterns/src/main.rs
@@ -43,5 +43,64 @@ fn main() {
         Point {x , y} => println!("On neither axis {} {}", x, y),
     }
 
+    // Destructuring Enums
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let msg = Message::ChangeColor(0, 106, 255);
+
+    match msg {
+        Message::Quit => println!("I Quit!"),
+        Message::Move { x, y } => println!("Move direction x: {}, y: {}", x, y),
+        Message::Write(x) => println!("Writing string: {}", x),
+        Message::ChangeColor(x, y, z) => println!("Changing color rgb: {}, {}, {}", x, y, z)
+    }
+
+    // Destructuring References
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+
+    // Rust 1.27 allows Point {x,y} to not need to explicitly show reference in the iter
+    let sum_of_squares: i32 =
+        points.iter().map(|Point { x, y }| x * x + y * y).sum();
+
+    println!("Sum of squares of the points: {}", sum_of_squares);
+
+
+    // Destructuring Structs and Tuples
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: 10 });
+    println!("Feet: {}, Inches: {}, Points x: {}, y: {}", feet, inches, x, y);
+
+
+    // Ignore Values in a pattern
+    let something = Some(5);
+    let something2 = Some(10);
+
+    match (something, something2) {
+        (Some(_), Some(_)) => println!("There was SOMETHING!"),
+        _ => println!("There was NOTHING!")
+    }
+
+    struct Point2 { x: i32, y: i32, z: i32 }
+    let origin = Point2 { x: 1, y: 2, z: 3 };
+
+    match origin {
+        Point2 { x, .. } => println!("Here's just x: {}", x),
+        _ => println!("There's no Point!")
+    }
+
+    let nums = (1, 2, 3, 4, 5);
+    match nums {
+        (first, .., last) => println!("Here the first and last nums: {}, {}", first, last)
+    }
+
 
 }
