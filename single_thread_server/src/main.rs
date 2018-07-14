719945d6a53266b4ec5019cebf3a9b564d3a60e7
Paul Naranja <paul@studyblue.com>
2018-07-13 22:18:28 -0700

    Add html response

--- single_thread_server/src/main.rs
+++ single_thread_server/src/main.rs
@@ -1,19 +1,21 @@
+use std::fs::File;
+use std::io::Error;
+use std::io::prelude::Read;
+use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::Read;
-use std::io::Error;
-use std::io::Write;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    listener.incoming().into_iter()
-        .for_each(|stream | {
-            let a = handle_connection(stream.expect("Was not able to get connection"))
-                .expect("Could not read stream");
-            return_response(a).expect("Could not write to stream");
-        });
+    let resp_contents = read_file("hello.html").expect("Could not read html file");
 
+    listener.incoming().into_iter()
+        .map(|stream| handle_connection(stream.expect("Was not able to get connection"))
+            .expect("Could not read stream"))
+        .for_each(|stream|
+            return_response(stream, resp_contents.clone()).expect("Could not write to stream")
+        );
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<TcpStream, Error> {
@@ -26,9 +28,17 @@ fn handle_connection(mut stream: TcpStream) -> Result<TcpStream, Error> {
     Ok(stream)
 }
 
-fn return_response(mut stream: TcpStream) -> Result<(), Error>{
-    let resp = "HTTP/1.1 200 OK\r\n\r\nHello!";
-    stream.write(resp.as_bytes())?;
+fn read_file(file_loc: &str) -> Result<String, Error> {
+    let mut file = File::open(file_loc)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
+fn return_response(mut stream: TcpStream, resp: String) -> Result<(), Error> {
+    let full_resp = format!("HTTP/1.1 200 OK\r\n\r\n{}", resp);
+    stream.write(full_resp.as_bytes())?;
     stream.flush()?;
     Ok(())
 }
