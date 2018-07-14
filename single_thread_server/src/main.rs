373c8ea1e36b32dbe5346ec85e479a754331f4f3
Paul Naranja <paul@studyblue.com>
2018-07-13 22:44:52 -0700

    Validate request and send hello or 404 html

--- single_thread_server/src/main.rs
+++ single_thread_server/src/main.rs
@@ -8,24 +8,25 @@ use std::net::TcpStream;
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    let resp_contents = read_file("hello.html").expect("Could not read html file");
 
     listener.incoming().into_iter()
-        .map(|stream| handle_connection(stream.expect("Was not able to get connection"))
-            .expect("Could not read stream"))
-        .for_each(|stream|
+        .map(|stream| stream.expect("Was not able to get connection"))
+        .map(|stream| handle_connection(stream).expect("Could not read stream"))
+        .for_each(|(stream, resp_contents)|
             return_response(stream, resp_contents.clone()).expect("Could not write to stream")
         );
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<TcpStream, Error> {
+fn handle_connection(mut stream: TcpStream) -> Result<(TcpStream, String), Error> {
     let mut buffer = [0; 512];
     stream.read(&mut buffer)?;
 
-    let request_str = String::from_utf8_lossy(&buffer[..]);
-    println!("Request: \n{}", request_str);
+    let readfile = |filename| read_file(filename).expect("Could not read html file");
 
-    Ok(stream)
+    let resp_contents =
+        if buffer.starts_with(b"GET / HTTP/1.1\r\n") { readfile("hello.html") } else { readfile("404.html") };
+
+    Ok((stream, resp_contents))
 }
 
 fn read_file(file_loc: &str) -> Result<String, Error> {
